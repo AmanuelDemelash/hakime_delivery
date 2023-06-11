15e81b4431640798ecc98d17ff22af74337d42a1
Amanuel Demelash <amanueldemelash12@gmail.com>
2023-06-11 15:52:02 -0700

    active order detail

--- lib/screens/delivery/activeorderdetail.dart
+++ lib/screens/delivery/activeorderdetail.dart
@@ -50,15 +50,10 @@ class ActiveOrderDetail extends StatelessWidget {
                   );
                 }
                 Map<String, dynamic> oreder = result.data!["orders_by_pk"];
+
                 return Stack(
                   children: [
-                    SizedBox(
-                      width: Get.width,
-                      height: Get.height,
-                    ),
-                       SizedBox(
-                         height: Get.height,
-                         child: Obx(() {
+                       Obx(() {
                             return
                               GoogleMap(
                               mapType: MapType.normal,
@@ -114,7 +109,7 @@ class ActiveOrderDetail extends StatelessWidget {
                                 Get.find<OrderController>().getpolyline(oreder);
                               },
                             );
-                          }),
+                          }
                        ),
 
                     // face problem
@@ -137,15 +132,73 @@ class ActiveOrderDetail extends StatelessWidget {
                         ),
                       ),
                     ),
-                    // user data
+
+                    Obx(() {
+                      return
+                        GoogleMap(
+                          mapType: MapType.normal,
+                          myLocationEnabled: true,
+                          initialCameraPosition: CameraPosition(
+                            target: LatLng(
+                                Get.find<Locationcontrollers>()
+                                    .current_lat
+                                    .value,
+                                Get.find<Locationcontrollers>()
+                                    .current_long
+                                    .value),
+                            zoom: 17.4746,
+                          ),
+                          markers: {
+                            Marker(
+                              markerId: const MarkerId("ph"),
+                              position: LatLng(
+                                  oreder["pharmacy"]["address"]["latitude"],
+                                  oreder["pharmacy"]["address"]["longitude"]),
+                              icon: Get.find<OrderController>().pharm_marker,
+                            ),
+                            Marker(
+                                markerId: const MarkerId("user"),
+                                position: LatLng(
+                                    oreder["order_address"]["latitude"],
+                                    oreder["order_address"]["longitude"]),
+                                icon: Get.find<OrderController>().user_marker),
+                            Marker(
+                                markerId: const MarkerId("delvery"),
+                                position: LatLng(
+                                    Get.find<Locationcontrollers>()
+                                        .current_lat
+                                        .value,
+                                    Get.find<Locationcontrollers>()
+                                        .current_long
+                                        .value),
+                                icon:
+                                Get.find<OrderController>().delivery_marker)
+                          },
+                          polylines: {
+                            Polyline(
+                                polylineId: const PolylineId("rout"),
+                                color: Constants.primcolor,
+                                width: 7,
+                                points: Get.find<OrderController>()
+                                    .polylinecordinates
+                                    .value)
+                          }
+                          ,
+                          onMapCreated: (GoogleMapController controller) {
+                            _controller.complete(controller);
+                            Get.find<OrderController>().getpolyline(oreder);
+                          },
+                        );
+                    }
+                    ),
+
                     Positioned(
                         bottom: 0,
                         left: 0,
                         right: 0,
                         child: Container(
-                          height: 160,
+
                           width: Get.width,
-                          padding: const EdgeInsets.all(10),
                           decoration: const BoxDecoration(
                               color: Constants.primcolor,
                               borderRadius: BorderRadius.only(
@@ -157,22 +210,22 @@ class ActiveOrderDetail extends StatelessWidget {
                               leading: ClipRRect(
                                 borderRadius: BorderRadius.circular(40),
                                 child: oreder["user"]["profile_image"]["url"] ==
-                                        null
+                                    null
                                     ? const Text("")
                                     : CachedNetworkImage(
-                                        imageUrl: oreder["user"]
-                                            ["profile_image"]["url"],
-                                        width: 50,
-                                        height: 50,
-                                        placeholder: (context, url) => Icon(
-                                          Icons.image,
-                                          color: Constants.whitesmoke
-                                              .withOpacity(0.5),
-                                        ),
-                                        errorWidget: (context, url, error) =>
-                                            const Icon(Icons.error),
-                                        fit: BoxFit.cover,
-                                      ),
+                                  imageUrl: oreder["user"]
+                                  ["profile_image"]["url"],
+                                  width: 50,
+                                  height: 50,
+                                  placeholder: (context, url) => Icon(
+                                    Icons.image,
+                                    color: Constants.whitesmoke
+                                        .withOpacity(0.5),
+                                  ),
+                                  errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                               title: Text(
                                 oreder["user"]["full_name"],
@@ -189,86 +242,82 @@ class ActiveOrderDetail extends StatelessWidget {
                             const SizedBox(
                               height: 10,
                             ),
+                            Container(
+                              width: Get.width,
+                              height: 70,
+                              padding: const EdgeInsets.only(
+                                  left: 20, right: 20, top: 10, bottom: 10),
+                              decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(20),
+                                      topRight: Radius.circular(20))),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  // call
+                                  Expanded(
+                                    child: SizedBox(
+                                      width: Get.width,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: ElevatedButton.icon(
+                                          icon: const Icon(Icons.phone,color: Colors.white,),
+                                          style: ElevatedButton.styleFrom(
+                                              backgroundColor: Constants.primcolor
+                                                  .withOpacity(0.8),
+
+                                              elevation: 0,
+                                              padding: const EdgeInsets.all(15)),
+                                          onPressed: () async {
+                                            launch(
+                                                "tel://${oreder["user"]["phone_number"]}");
+                                          },
+                                          label: const Text("Call",style: TextStyle(color: Colors.white),),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 6,
+                                  ),
+                                  // deleverd
+                                  Expanded(
+                                    child: SizedBox(
+                                      width: Get.width,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: ElevatedButton.icon(
+                                          icon: const Icon(Icons.check_circle,color: Colors.white,),
+                                          style: ElevatedButton.styleFrom(
+                                              elevation: 0,
+                                              backgroundColor: Colors.red,
+                                              padding: const EdgeInsets.all(15)),
+                                          onPressed: () {
+                                            Get.toNamed("/complate", arguments: {
+                                              "id": _order_id,
+                                              "phname": oreder["pharmacy"]["name"],
+                                              "phloc": oreder["pharmacy"]["address"]
+                                              ["location"],
+                                              "phimage": oreder["pharmacy"]
+                                              ["logo_image"]["url"],
+                                              "date": oreder["created_at"],
+                                              "dcost": oreder["delivery_fee"],
+                                              "tcost": oreder["total_cost"],
+                                              "code": oreder["order_code"]
+                                            });
+                                          },
+                                          label: const Text("Delivered",style: TextStyle(color: Colors.white),),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+
                           ]),
                         )),
-                    Positioned(
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Container(
-                          width: Get.width,
-                          height: 70,
-                          padding: const EdgeInsets.only(
-                              left: 20, right: 20, top: 10, bottom: 10),
-                          decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(20),
-                                  topRight: Radius.circular(20))),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              // call
-                              Expanded(
-                                child: SizedBox(
-                                  width: Get.width,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: ElevatedButton.icon(
-                                      icon: const Icon(Icons.phone,color: Colors.white,),
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Constants.primcolor
-                                              .withOpacity(0.8),
-
-                                          elevation: 0,
-                                          padding: const EdgeInsets.all(15)),
-                                      onPressed: () async {
-                                        launch(
-                                            "tel://${oreder["user"]["phone_number"]}");
-                                      },
-                                      label: const Text("Call",style: TextStyle(color: Colors.white),),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 6,
-                              ),
-                              // deleverd
-                              Expanded(
-                                child: SizedBox(
-                                  width: Get.width,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: ElevatedButton.icon(
-                                      icon: const Icon(Icons.check_circle,color: Colors.white,),
-                                      style: ElevatedButton.styleFrom(
-                                          elevation: 0,
-                                          backgroundColor: Colors.red,
-                                          padding: const EdgeInsets.all(15)),
-                                      onPressed: () {
-                                        Get.toNamed("/complate", arguments: {
-                                          "id": _order_id,
-                                          "phname": oreder["pharmacy"]["name"],
-                                          "phloc": oreder["pharmacy"]["address"]
-                                              ["location"],
-                                          "phimage": oreder["pharmacy"]
-                                              ["logo_image"]["url"],
-                                          "date": oreder["created_at"],
-                                          "dcost": oreder["delivery_fee"],
-                                          "tcost": oreder["total_cost"],
-                                          "code": oreder["order_code"]
-                                        });
-                                      },
-                                      label: const Text("Delivered",style: TextStyle(color: Colors.white),),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                    ),
 
 
                   ],
